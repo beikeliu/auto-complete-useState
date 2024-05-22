d523417455d1afaa5024a13182592e609402f96e
wb_liubeike <wb_liubeike@meituan.com>
2024-05-22 17:40:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# transformusestate README
+
+在vscode中选中const [xxx,]，使用快捷键shift+command+p打开命令行输入transform useState，即可将本行(或多行)变更为const [xxx,setXxx] = useState();
---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.transformUseState",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const selections = editor.selections;
+
+      editor.edit((editBuilder) => {
+        selections.forEach((selection) => {
+          const text = document.getText(selection);
+					const regex = /const \[(\w+),\s*\]/g;
+          const newText = text.replace(regex, (match, p1) => {
+						const capitalized = p1.charAt(0).toUpperCase() + p1.slice(1);
+						return `const [${p1}, set${capitalized}] = useState();`;
+				});
+
+          editBuilder.replace(selection, newText);
+        });
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
